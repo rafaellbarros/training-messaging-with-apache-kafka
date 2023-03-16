25875adea2b174468a93c076e63e3a12dcd49065
Rafael <rafaelbarros.df@gmail.com>
2023-03-16 20:31:43 -0300

    :construction: Various consumers and producers

--- kafka-producers-consumers-streams/ecommerce/src/main/java/br/com/rafaellbarros/ecommerce/NewOrderMain.java
+++ kafka-producers-consumers-streams/ecommerce/src/main/java/br/com/rafaellbarros/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.rafaellbarros.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -17,20 +18,24 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        final var producer = new KafkaProducer<String, String>(properities());
-        final var value = "132123, 67523, 1234";
-        final var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "132123,67523,1234";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition "+ data.partition() + "/ offset "
-                    + "data.offset()" + "/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
-    private static Properties properities() {
+
+    private static Properties properties() {
         final var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
